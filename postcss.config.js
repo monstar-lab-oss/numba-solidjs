512ea70bd11ce872d3952a2d44bc50ffb325d0bf
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-07-13 14:19:44 +0900

    adjust tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/index.html", "./src/components/*.{tsx,css}"],
+  purge: ["./src/index.html", "./src/components/**/*.{tsx,css}"],
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
