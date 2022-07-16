3ce93313355015c4428426158476c6f9d15fec18
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-07-17 00:53:58 +0900

    refactor: button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,10 @@
-import type { Component, JSX } from "solid-js";
-import { splitProps } from "solid-js";
+import { splitProps, Component, JSX } from "solid-js";
 
-export type Props = {
-  title: string;
-} & JSX.HTMLAttributes<HTMLButtonElement>;
+type Props = {
+  children: JSX.Element;
+} & JSX.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: Component<Props> = (props) => {
-  const [, attributes] = splitProps(props, ["title"]);
-
-  return (
-    <button
-      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-      {...attributes}
-    >
-      {props.title}
-    </button>
-  );
+  const [, attributes] = splitProps(props, ["children"]);
+  return <button {...attributes}>{props.children}</button>;
 };
