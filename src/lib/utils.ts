6fba438fcd72381afe749f95ff2a417f58571301
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-07-19 16:14:26 +0900

    drop spread operator for now

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,17 @@ export function capitalize(str: string): string {
 export function dashify(str: string): string {
   return str.replace(/([a-zA-Z])(?=[A-Z\d])/g, "$1-").toLowerCase();
 }
-
-export const figmaRGBA = ({ r = 0, g = 0, b = 0, a = undefined }) => {
-  const color = { r: r / 255, g: g / 255, b: b / 255 };
-  return a ? { ...color, a } : color;
+// TODO: use spread operator
+// e.g export const figmaRGBA = ({ r = 0, g = 0, b = 0, a = undefined }) => {
+//   const color = { r: r / 255, g: g / 255, b: b / 255 };
+//   return a ? { ...color, a } : color
+// }
+export const figmaRGBA = (rgba: {
+  r: number;
+  g: number;
+  b: number;
+  a?: number;
+}) => {
+  const color = { r: rgba.r / 255, g: rgba.g / 255, b: rgba.b / 255 };
+  return color;
 };
