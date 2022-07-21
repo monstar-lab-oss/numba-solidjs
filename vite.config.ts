91a7fc50b63641528ea04ec6d9d30a43e258645a
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-07-21 10:34:56 +0900

    adjust tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import path from "path";
