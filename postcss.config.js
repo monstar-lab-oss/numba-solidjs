d4d6ce9c91e8872c4e9f38651eac72090c395f7c
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-08-11 18:39:35 +0900

    introduce postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  // TODO: Make sure we need the `purge` option
   purge: ["./src/index.html", "./src/components/**/*.{tsx,css}"],
   plugins: {
+    "postcss-import": {},
     tailwindcss: {},
     autoprefixer: {},
   },
