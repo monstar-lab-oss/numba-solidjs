d98038dde9c6ed2baa7447f5164d6c4f8cb41472
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-08-19 16:32:41 +0900

    refactor: styles to css file

--- src/components/GroupTable.tsx
+++ src/components/GroupTable.tsx
@@ -1,7 +1,7 @@
-import { Component, JSX, For } from "solid-js";
+import { Component, JSX, For, splitProps } from "solid-js";
 import type { Group } from "@/types/Group";
-import { splitProps } from "solid-js";
 import { clsx } from "clsx";
+import css from "./GroupTable.module.css";
 
 export type Props = {
   data: Group[];
@@ -35,8 +35,8 @@ export const GroupTable: Component<Props> = (props) => {
 
   return (
     <div class={clsx({ "w-full": true })} {...attributes}>
-      <table class="w-full text-left text-xs text-gray-500 dark:text-gray-400">
-        <thead class="bg-gray-50 text-xs capitalize text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+      <table class={clsx({ [css.style]: true })}>
+        <thead>
           <tr class="h-12">
             <th scope="col" class="p-4">
               name
@@ -50,23 +50,12 @@ export const GroupTable: Component<Props> = (props) => {
               <tr
                 onClick={(e) => onSelectClick(e, item.id)}
                 class={clsx({
-                  "cursor-pointer border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600":
-                    true,
-                  "bg-gray-50": props.selectedGroup === item.id,
+                  [css.selected_row]: props.selectedGroup === item.id,
                 })}
               >
-                <th
-                  scope="row"
-                  class="w-48 px-4 font-medium text-gray-900 dark:text-white"
-                >
-                  {item.name}
-                </th>
-                <td class="flex items-center py-4 px-4">
-                  <a
-                    href="#"
-                    onClick={(e) => onRemoveClick(e, item.id)}
-                    class="font-medium text-red-600 hover:underline dark:text-red-500"
-                  >
+                <th scope="row">{item.name}</th>
+                <td>
+                  <a href="#" onClick={(e) => onRemoveClick(e, item.id)}>
                     Remove
                   </a>
                 </td>
