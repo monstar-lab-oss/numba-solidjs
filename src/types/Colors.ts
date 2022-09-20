53deddf6d06deb374123c4f63add55aa8ade1cad
yuta-hidaka <yuta322@gmail.com>
2022-09-20 17:15:35 +0900

    Add comment

--- src/types/Colors.ts
+++ src/types/Colors.ts
@@ -3,6 +3,7 @@ export type Color =
   | "primary"
   | "danger"
   | "secondary"
+  // NOTE: Outline is not a color but for now we don't consider so far.
   | "primaryOutline"
   | "dangerOutline"
   | "secondaryOutline";
