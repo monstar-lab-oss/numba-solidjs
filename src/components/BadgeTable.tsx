de89019ce56231ee357a4afd2542bca7230aa57e
yuta-hidaka <yuta322@gmail.com>
2022-10-12 15:26:42 +0900

    Fix scroll issue

--- src/components/BadgeTable.tsx
+++ src/components/BadgeTable.tsx
@@ -144,7 +144,8 @@ export const BadgeTable: Component<Props> = (props) => {
                         onChange={() => onToggleClick(item.id)}
                         class="h-4 w-4 cursor-pointer rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
                       />
-                      <label for="checkbox-table-search-1">checkbox</label>
+                      {/* FIXME this label with class 'sr-only' occurred unexpected scroll effect.  */}
+                      {/* <label for="checkbox-table-search-1">checkbox</label> */}
                     </div>
                   </td>
                   <th colSpan={2} scope="row">
