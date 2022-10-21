e7fe1f19f05fb2abfcd03db68eee47422c71bac4
yuta-hidaka <yuta322@gmail.com>
2022-10-21 18:47:21 +0900

    Remove dark mode theme and replace color to numba color

--- src/components/BadgeTable.tsx
+++ src/components/BadgeTable.tsx
@@ -95,7 +95,7 @@ export const BadgeTable: Component<Props> = (props) => {
                     type="checkbox"
                     checked={isSelectAll()}
                     onChange={() => onToggleAllClick()}
-                    class="h-4 w-4 cursor-pointer rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                    class="h-4 w-4 cursor-pointer rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500"
                   />
                   <label
                     for="checkbox-all-search"
@@ -146,7 +146,7 @@ export const BadgeTable: Component<Props> = (props) => {
                         type="checkbox"
                         checked={item.selected()}
                         onChange={() => onToggleClick(item.id)}
-                        class="h-4 w-4 cursor-pointer rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                        class="h-4 w-4 cursor-pointer rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500"
                       />
                       {/* FIXME this label with class 'sr-only' occurred unexpected scroll effect.  */}
                       {/* <label for="checkbox-table-search-1">checkbox</label> */}
