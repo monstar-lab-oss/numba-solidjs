2f3f612daf3d3d40bae4ed01cb751a189e2b26a7
Yuta Hidaka <46583585+yuta-hidaka@users.noreply.github.com>
2023-01-31 12:25:57 +0900

    test: Carousel story style

--- src/components/Carousel.stories.tsx
+++ src/components/Carousel.stories.tsx
@@ -4,13 +4,22 @@ import page3 from "@/asset/tutorial/page-3.png";
 import page4 from "@/asset/tutorial/page-4.png";
 import { Carousel, Props } from "@/components/Carousel";
 import { Text } from "@/components/Text";
+import { UI_HEIGHT, UI_WIDTH } from "@/constants";
 import { Meta, Story } from "@storybook/html";
+
+const styles = {
+  transform: "scale(1)",
+  height: UI_HEIGHT + "px",
+  width: UI_WIDTH + "px",
+  "background-color": "white",
+};
 
 export default {
   title: "Components/Carousel",
   args: {
     placeholder: "Placeholder",
   },
+  decorators: [(storyFn) => <div style={styles}>{storyFn()}</div>],
 } as Meta;
 
 const contents = [
@@ -60,12 +69,7 @@ const contents = [
 
 // @ts-expect-error FIXME: Should return Solid component
 const Template: Story<Props> = (args) => {
-  return (
-    // NOTE: import { UI_WIDTH, UI_HEIGHT } from "@/constants";
-    <div class={`bg-white w-[480px] h-[392px]`}>
-      <Carousel {...args} />
-    </div>
-  );
+  return <Carousel {...args} />;
 };
 
 export const Default = Template.bind({});
