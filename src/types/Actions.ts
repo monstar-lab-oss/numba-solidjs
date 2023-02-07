d9d16ec6fbd3740253840ccb47db36fe99361645
Yuta Hidaka <46583585+yuta-hidaka@users.noreply.github.com>
2023-02-07 17:15:21 +0900

    refactor: un-export

--- src/types/Actions.ts
+++ src/types/Actions.ts
@@ -1,7 +1,7 @@
 import type { Badge } from "@/types/Badge";
 import type { Group } from "@/types/Group";
 
-export type NodeId = string;
+type NodeId = string;
 
 export type UpdateStorePayload = {
   selectedGroupID?: string;
