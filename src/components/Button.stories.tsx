729263113d26bd092f6f7e429e911b7cb99d7f29
Yuta Hidaka <46583585+yuta-hidaka@users.noreply.github.com>
2023-06-19 16:50:39 +0900

    refactor: migrate storybook 6.x to 7.x

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,67 +1,75 @@
-import { Meta, Story } from "@storybook/html";
-import { Button, Props } from "./Button";
+import type { Meta } from "storybook-solidjs";
+import { Button } from "./Button";
 
-export default {
+const meta = {
+  component: Button,
   title: "Components/Button",
   args: {
     children: "Button",
   },
-} as Meta;
+} satisfies Meta<typeof Button>;
+export default meta;
 
-// @ts-expect-error FIXME: Should return Solid component
-const Template: Story<Props> = (args) => {
-  return <Button {...args}>{args.children}</Button>;
+export const Default = {
+  args: {},
 };
 
-export const Default = Template.bind({});
-Default.args = {};
-
-export const Primary = Template.bind({});
-Primary.args = {
-  color: "primary",
+export const Primary = {
+  args: {
+    color: "primary",
+  },
 };
 
-export const Danger = Template.bind({});
-Danger.args = {
-  color: "danger",
+export const Danger = {
+  args: {
+    color: "danger",
+  },
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  color: "disabled",
+export const Disabled = {
+  args: {
+    color: "disabled",
+  },
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  color: "secondary",
+export const Secondary = {
+  args: {
+    color: "secondary",
+  },
 };
 
-export const White = Template.bind({});
-White.args = {
-  color: "white",
+export const White = {
+  args: {
+    color: "white",
+  },
 };
 
-export const DarkGray = Template.bind({});
-DarkGray.args = {
-  color: "darkGray",
+export const DarkGray = {
+  args: {
+    color: "darkGray",
+  },
 };
 
-export const PrimaryOutline = Template.bind({});
-PrimaryOutline.args = {
-  color: "primaryOutline",
+export const PrimaryOutline = {
+  args: {
+    color: "primaryOutline",
+  },
 };
 
-export const DangerOutline = Template.bind({});
-DangerOutline.args = {
-  color: "dangerOutline",
+export const DangerOutline = {
+  args: {
+    color: "dangerOutline",
+  },
 };
 
-export const SecondaryOutline = Template.bind({});
-SecondaryOutline.args = {
-  color: "secondaryOutline",
+export const SecondaryOutline = {
+  args: {
+    color: "secondaryOutline",
+  },
 };
 
-export const Link = Template.bind({});
-Link.args = {
-  link: true,
+export const Link = {
+  args: {
+    link: true,
+  },
 };
--- src/components/Confirm.stories.tsx
+++ src/components/Confirm.stories.tsx
@@ -1,137 +1,144 @@
-import { Meta, Story } from "@storybook/html";
 import { userEvent, within } from "@storybook/testing-library";
-import { Confirm, Props } from "./Confirm";
+import type { Meta, StoryObj } from "storybook-solidjs";
+import { Confirm } from "./Confirm";
 
-export default {
+type Story = StoryObj<typeof meta>;
+
+const meta = {
+  component: Confirm,
   title: "Components/Confirm",
   args: {
     children: "Confirm",
   },
-} as Meta;
+} satisfies Meta<typeof Confirm>;
+export default meta;
 
-// @ts-expect-error FIXME: Should return Solid component
-const Template: Story<Props> = (args) => <Confirm {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  onConfirm: () => console.log("onConfirm"),
-  onClose: () => console.log("onClose"),
-  body: "Confirm component",
-  confirmButtonColor: "primary",
-  confirmButtonText: "Default(Primary)",
-  cancelButtonText: "Cancel",
+export const Default: Story = {
+  args: {
+    onConfirm: () => console.log("onConfirm"),
+    onClose: () => console.log("onClose"),
+    body: "Confirm component",
+    confirmButtonColor: "primary",
+    confirmButtonText: "Default(Primary)",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const ConfirmHidden = Template.bind({});
-ConfirmHidden.args = {
-  onConfirm: () => console.log("onConfirm"),
-  onClose: () => console.log("onClose"),
-  body: "Confirm component",
-  confirmButtonColor: "primary",
-  confirmButtonText: "Confirm Hidden",
-  cancelButtonText: "Cancel",
+export const ConfirmHidden: Story = {
+  args: {
+    onConfirm: () => console.log("onConfirm"),
+    onClose: () => console.log("onClose"),
+    body: "Confirm component",
+    confirmButtonColor: "primary",
+    confirmButtonText: "Confirm Hidden",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const Danger = Template.bind({});
-Danger.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "danger",
-  confirmButtonText: "Danger",
-  cancelButtonText: "Cancel",
+export const Danger: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "danger",
+    confirmButtonText: "Danger",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "disabled",
-  confirmButtonText: "Disabled",
-  cancelButtonText: "Cancel",
+export const Disabled: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "disabled",
+    confirmButtonText: "Disabled",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "secondary",
-  confirmButtonText: "Secondary",
-  cancelButtonText: "Cancel",
+export const Secondary: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "secondary",
+    confirmButtonText: "Secondary",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const White = Template.bind({});
-White.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "white",
-  confirmButtonText: "White",
-  cancelButtonText: "Cancel",
+export const White: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "white",
+    confirmButtonText: "White",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const PrimaryOutline = Template.bind({});
-PrimaryOutline.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "primaryOutline",
-  confirmButtonText: "Primary Outline",
-  cancelButtonText: "Cancel",
+export const PrimaryOutline: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "primaryOutline",
+    confirmButtonText: "Primary Outline",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const DangerOutline = Template.bind({});
-DangerOutline.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "dangerOutline",
-  confirmButtonText: "Danger Outline",
-  cancelButtonText: "Cancel",
+export const DangerOutline: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "dangerOutline",
+    confirmButtonText: "Danger Outline",
+    cancelButtonText: "Cancel",
+  },
 };
 
-export const SecondaryOutline = Template.bind({});
-SecondaryOutline.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "secondaryOutline",
-  confirmButtonText: "Secondary Outline",
-  cancelButtonText: "Cancel",
+export const SecondaryOutline: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "secondaryOutline",
+    confirmButtonText: "Secondary Outline",
+    cancelButtonText: "Cancel",
+  },
 };
 
 // TEST
-
-export const ConfirmClick = Template.bind({});
-
-ConfirmClick.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "secondaryOutline",
-  confirmButtonText: "Confirm",
-  cancelButtonText: "Cancel",
+export const ConfirmClick: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "secondaryOutline",
+    confirmButtonText: "Confirm",
+    cancelButtonText: "Cancel",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole("button", { name: "Confirm" }));
+  },
 };
 
-ConfirmClick.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  await userEvent.click(canvas.getByRole("button", { name: "Confirm" }));
-};
-
-export const ConfirmCancel = Template.bind({});
-
-ConfirmCancel.args = {
-  onConfirm: () => console.log("OnConfirm fired !"),
-  onClose: () => console.log("OnClose fired !"),
-  body: "Confirm component",
-  confirmButtonColor: "secondaryOutline",
-  confirmButtonText: "Confirm",
-  cancelButtonText: "Cancel",
-};
-
-ConfirmCancel.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  await userEvent.click(canvas.getByRole("button", { name: "Cancel" }));
+export const ConfirmCancel: Story = {
+  args: {
+    onConfirm: () => console.log("OnConfirm fired !"),
+    onClose: () => console.log("OnClose fired !"),
+    body: "Confirm component",
+    confirmButtonColor: "secondaryOutline",
+    confirmButtonText: "Confirm",
+    cancelButtonText: "Cancel",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole("button", { name: "Cancel" }));
+  },
 };
